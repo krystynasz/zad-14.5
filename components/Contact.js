1244d894db831de007986e8a550902759383a736
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-07-26 18:32:15 +0200

    Add project files

---
+++ components/Contact.js
@@ -0,0 +1,22 @@
+
+  var Contact = React.createClass({
+    propTypes: {
+      item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+      return (
+        React.createElement('div', {className: 'contactItem'},
+          React.createElement('img', {
+            className: 'contactImage',
+            src: 'https://www.shareicon.net/download/2015/08/23/89482_contact.svg'
+          }),
+          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+            this.props.item.email
+          )
+        )
+      )
+    },
+  });
